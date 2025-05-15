447a8f3e76fd75e6e42e7eed3df213e3bd1714be
ftbeat <downgabe@list.ru>
2025-05-15 16:12:53 +0300

    tablet-version

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,27 @@
+// Header.tsx
+import { useState, useEffect } from 'react'
 import styles from './Header.module.scss'
 import LogoHeader from '../../assets/icons/LogoHeader'
+import BurgerIcon from '../../assets/icons/BurgerIcon'
+import CloseIcon from '../../assets/icons/CloseIcon'
 
 const Header = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+	useEffect(() => {
+		document.body.style.overflow = isMenuOpen ? 'hidden' : 'auto'
+	}, [isMenuOpen])
+
+	const toggleMenu = () => setIsMenuOpen(prev => !prev)
+
 	return (
 		<header className={styles.header}>
 			<div className={styles.headerContent}>
 				<div className={styles.logo}>
 					<LogoHeader />
 				</div>
-				<nav className={styles.nav}>
+
+				<nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}>
 					<ul>
 						<li>
 							<a href='#'>Overview</a>
@@ -27,10 +40,21 @@ const Header = () => {
 						</li>
 					</ul>
 				</nav>
+
 				<div className={styles.headerBtn}>
 					<button>Get Started</button>
 				</div>
+
+				{/* иконка-обёртка для анимации */}
+				<div
+					className={`${styles.burger} ${isMenuOpen ? styles.open : ''}`}
+					onClick={toggleMenu}
+				>
+					{isMenuOpen ? <CloseIcon /> : <BurgerIcon />}
+				</div>
 			</div>
+
+			{isMenuOpen && <div className={styles.overlay} onClick={toggleMenu} />}
 		</header>
 	)
 }
